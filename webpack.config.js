db519fb4ad86edd1e81d0dcea066395b1e2ffef4
keepgo-studio <keepgo.studio@gmail.com>
2022-12-14 01:39:59 +0900

    Fix issue for node environment

--- webpack.config.js
+++ webpack.config.js
@@ -2,64 +2,67 @@ const path = require("path");
 
 const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 const tsconfigPath = path.join(__dirname, "tsconfig.json");
 
-module.exports = {
-  mode: "development",
+module.exports = [
+  {
+    mode: "development",
 
-  target: "web",
+    target: "web",
 
-  entry: {
-    "bundle": path.resolve(__dirname, "./src", "./index.ts"),
-    "bundle.min": path.resolve(__dirname, "./src", "./index.ts"),
-  },
+    entry: {
+      "bundle": path.resolve(__dirname, "./src", "./index.ts"),
+      "bundle.min": path.resolve(__dirname, "./src", "./index.ts"),
+    },
 
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-        options: {
-          configFile: tsconfigPath,
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+          options: {
+            configFile: tsconfigPath,
+          },
         },
-      },
 
-      {
-        test: /.mp3$/,
-        loader: "file-loader",
-      },
-    ],
+        {
+          test: /.mp3$/,
+          loader: "file-loader",
+        },
+      ],
+    },
+
+    output: {
+      path: path.resolve(__dirname, "./dist/es"),
+      filename: "[name].js",
+      globalObject: "this",
+      library: "UI",
+      libraryTarget: "umd",
+      umdNamedDefine: true,
+    },
+
+    resolve: {
+      modules: ["node_modules"],
+
+      extensions: [".ts", ".js"],
+
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: tsconfigPath,
+        }),
+      ],
+    },
+
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          include: /\.min\.js$/,
+        }),
+      ],
+    },
   },
-
-  output: {
-    path: path.resolve(__dirname, "./dist/es"),
-    filename: "[name].js",
-    globalObject: "this",
-    library: "UI",
-    libraryTarget: "umd",
-    umdNamedDefine: true,
-  },
-
-  resolve: {
-    modules: ["node_modules"],
-
-    extensions: [".ts", ".js"],
-
-    plugins: [
-      new TsconfigPathsPlugin({
-        configFile: tsconfigPath,
-      }),
-    ],
-  },
-
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        include: /\.min\.js$/,
-      }),
-    ],
-  },
-};
+];
