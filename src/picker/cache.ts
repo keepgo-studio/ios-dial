8ad4f5513b23afc2bb4765b33f13a0b669a55d54
iougou03 <haesol820@naver.com>
2023-03-14 21:11:17 +0900

    1.3.2 complete, change color responsive

--- src/picker/cache.ts
+++ src/picker/cache.ts
@@ -2,7 +2,7 @@ class Cache {
   static async cache(mp3Src: string) {
     return await fetch(mp3Src)
       .then((v) => v.blob())
-      .catch(() => undefined);
+      .catch((err) => console.error('Cache:', err));
   }
 }
 
--- src/picker/index.ts
+++ src/picker/index.ts
@@ -17,14 +17,15 @@ const styles = `
   }
 
   #root {
-    background-color: #000;
+    background-color: transparent;
+    color: #fff;
+    mix-blend-mode: difference;
 
     padding: 0 20px;
   }
 
   .picker-container {
     position: relative;
-    color: #fff;
     width: 100%;
     height: 100%;
     display: flex;
